4d385ea979a0278cde5bd9029e0cac3ec6b2f39c
Mlockbug <energetichamster@gmail.com>
2023-01-13 11:49:53 +0000

    added 3D project

---
+++ IE-3D/IE-3D/Assets/Code/CharacterControl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
